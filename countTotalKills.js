d1a4d967a9a1501711a7ccbd5141c9568e31c3e9
Jefferson Sousa <sousa.webdesign@gmail.com>
2024-07-16 20:44:17 -0300

    Done

--- countTotalKills.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.countTotalKills = void 0;
-const countTotalKills = (matchLines) => {
-    const linesMatching = matchLines.filter((line) => line.match(' killed '));
-    return linesMatching.length;
-};
-exports.countTotalKills = countTotalKills;
