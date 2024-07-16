64b02b445c00f7a62176ec96edcd52c2b052347a
Jefferson Sousa <sousa.webdesign@gmail.com>
2024-07-16 20:46:35 -0300

    Done

--- countPlayerKills.spec.ts
+++ countPlayerKills.spec.ts
@@ -1,6 +1,6 @@
-const count = require("./countPlayerKills");
+const count1 = require("./countPlayerKills");
 
-const testData = [
+const testData1 = [
     `20:54 Kill: 1022 2 22: <world> killed Isgalamido by MOD_TRIGGER_HURT`,
     `22:06 Kill: 2 3 7: Isgalamido killed Mocinha by MOD_ROCKET_SPLASH`,
     `1:08 Kill: 3 2 6: Isgalamido killed Mocinha by MOD_ROCKET`
@@ -11,7 +11,7 @@ describe('countPlayerKills', () => {
 
     test('Should match the player kills length', () => {
 
-        expect(count.countPlayerKills('Isgalamido', testData)).toEqual(1);
+        expect(count1.countPlayerKills('Isgalamido', testData1)).toEqual(1);
 
     })
 
--- countTotalKills.spec.ts
+++ countTotalKills.spec.ts
@@ -1,6 +1,6 @@
-const count = require("./countTotalKills");
+const count2 = require("./countTotalKills");
 
-const testData = [
+const testData2 = [
     `20:54 Kill: 1022 2 22: <world> killed Isgalamido by MOD_TRIGGER_HURT`,
     `22:06 Kill: 2 3 7: Isgalamido killed Mocinha by MOD_ROCKET_SPLASH`,
     `1:08 Kill: 3 2 6: Isgalamido killed Mocinha by MOD_ROCKET`
@@ -10,7 +10,7 @@ describe('countTotalKills', () => {
 
     test('Should match the total kills length', () => {
 
-        expect(count.countTotalKills(testData)).toEqual(3);
+        expect(count2.countTotalKills(testData2)).toEqual(3);
 
     })
 
